cf25afacfefbe543b7f2692efc32dd4c90066f2b
krakissi <mike@krakissi.net>
2019-03-21 23:27:33 -0400

    bump rev v2.22b

--- ewcfg.py
+++ ewcfg.py
@@ -9,7 +9,7 @@ from ewmap import EwPoi
 from ewslimeoid import EwBody, EwHead, EwMobility, EwOffense, EwDefense, EwSpecial, EwBrain
 
 # Global configuration options.
-version = "v2.22b"
+version = "v2.22c"
 dir_msgqueue = 'msgqueue'
 
 # Update intervals
