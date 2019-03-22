2e6a0490257cec26f91b45de4c424ca3a4816d28
MeatyLearner <44686266+MeatyLearner@users.noreply.github.com>
2019-03-22 22:47:42 +0000

    minor fix

--- ewitem.py
+++ ewitem.py
@@ -760,7 +760,7 @@ async def discard(cmd):
 				if user_data.weaponmarried:
 					weapon = ewcfg.weapon_map.get(item.item_props.get("weapon_type"))
 					response = "As much as it would be satisfying to just chuck your {} down an alley and be done with it, here in civilization we deal with things *maturely.* You’ll have to speak to the guy that got you into this mess in the first place, or at least the guy that allowed you to make the retarded decision in the first place. Luckily for you, they’re the same person, and he’s at the Dojo.".format(weapon.str_weapon)
-					await ewutils.send_message(cmd.client, cmd.message.channel, ewutils.formatMessage(cmd.message.author, response))
+					return await ewutils.send_message(cmd.client, cmd.message.channel, ewutils.formatMessage(cmd.message.author, response))
 				else:
 					user_data.weapon = ""
 					user_data.persist()
