eafc8d6fb88dc221010b4a4c91626c3568c9386f
magicksRampage <marc.reichelt94@gmail.com>
2019-04-19 13:29:38 -0400

    lower bound exchangerate at 1

--- ewmarket.py
+++ ewmarket.py
@@ -675,7 +675,7 @@ def market_tick(stock_data, id_server):
 
 
 	# negative exchange rate causes problems, duh
-	exchange_rate_increase = max(exchange_rate_increase, -stock_data.exchange_rate)
+	exchange_rate_increase = max(exchange_rate_increase, -stock_data.exchange_rate + 1)
 
 	points = abs(exchange_rate_increase / 1000)
 
