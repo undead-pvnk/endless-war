bc150e30257df2e214f1ee6b87ef8d98988e0a77
marc-andre <marc.reichelt94@gmail.com>
2019-05-04 12:44:38 +0000

    reactivate transports

--- client.py
+++ client.py
@@ -479,7 +479,7 @@ async def on_ready():
 
 		asyncio.ensure_future(ewdistrict.capture_tick_loop(id_server = server.id))
 		asyncio.ensure_future(ewutils.bleed_tick_loop(id_server = server.id))
-		#await ewtransport.init_transports(id_server = server.id)
+		await ewtransport.init_transports(id_server = server.id)
 
 	try:
 		ewutils.logMsg('Creating message queue directory.')
