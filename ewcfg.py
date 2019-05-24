2d4540ab9020bcfd5b4fad07034dc0376cf62651
munchypizza <munchywearstinyhats@gmail.com>
2019-05-23 22:58:27 -0700

    -blargh

--- ewcfg.py
+++ ewcfg.py
@@ -12,7 +12,7 @@ from ewquadrants import EwQuadrantFlavor
 from ewtransport import EwTransportLine
 
 # Global configuration options.
-version = "v3.2"
+version = "MILLING"
 dir_msgqueue = 'msgqueue'
 
 # Update intervals
