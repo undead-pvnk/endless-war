b8033dde36b8e3f4d5628b341eb21c9d151edcf1
munchypizza <munchywearstinyhats@gmail.com>
2019-05-27 13:53:55 -0400

    -blargh

--- ewcmd.py
+++ ewcmd.py
@@ -601,13 +601,6 @@ async def saturateslimeoid(cmd):
 	elif item_sought:
 
 		dye = item_sought.subcontext
-
-		for result in ewcfg.dye_list:
-			if result.subcontext != item_search:
-				pass
-			else:
-				dye.append(result)
-
 		hue = ewcfg.hue_map.get(dye)
 		response = " {}".format(hue.str_saturate)
 		slimeoid.body = hue.id_hue
