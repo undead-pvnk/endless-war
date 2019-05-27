76ea52c7437de30ba412a936aa8e07ebe82c819d
munchypizza <munchywearstinyhats@gmail.com>
2019-05-27 17:38:56 -0400

    -blargh

--- ewfarm.py
+++ ewfarm.py
@@ -236,7 +236,7 @@ async def mill(cmd):
 					id_user = cmd.message.author.id,
 					id_server = cmd.message.server.id,
 					item_props = {
-						'id_item': item.id_defaultitem,
+						'id_item': item.id_item,
 						'context': item.context,
 						'subcontext': item.subcontext,
 						'item_name': item.str_name,
