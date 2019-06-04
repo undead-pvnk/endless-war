ea1a1f49a1d562abdbe8e3c50a48bcaf64ebe630
magicksRampage <marc.reichelt94@gmail.com>
2019-06-03 21:59:57 -0400

    fixed negaslimeoid name length

--- ewspooky.py
+++ ewspooky.py
@@ -247,7 +247,7 @@ def generate_negaslimeoid_name():
 	titles = ["Angel", "Emissary", "Gaping Maw", "Apostle", "Nemesis", "Harbinger", "Reaper", "Incarnation", "Wanderer", "Berserker", "Outcast", "Monarch", "Anomaly"]
 	domains = ["Curses", "Doom", "Oblivion", "Darkness", "Madness", "the Void", "the Deep", "Nightmares", "Wrath", "Pestilence", "the End", "Terror", "Sorrow", "Pain", "Despair", "Souls", "Secrets", "Ruin", "Hatred", "Shadows", "the Night"]
 	title = "{} of {}".format(random.choice(titles), random.choice(domains))
-	name_length = random.randrange(5,min(10,31-len(title)))
+	name_length = random.randrange(5,min(10,30-len(title)))
 	consonants = random.choice(["chlt","crwx","fhlt","bghl","brpq"])
 	vowels = "aeuuooyy"
 	num_vowels = random.randrange(int(name_length / 4), int(name_length/3)+1)
