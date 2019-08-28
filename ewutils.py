28859315cb8318c8c4c958c2344f2881627ad852
marc-andre <marc.reichelt94@gmail.com>
2019-08-28 17:35:41 +0000

    fixed otp roles

--- ewutils.py
+++ ewutils.py
@@ -1215,5 +1215,5 @@ async def explode(damage = 0, district_data = None):
 	return resp_cont
 
 def is_otp(user_data):
-	return user_data.poi in [ewcfg.poi_id_thesewers, ewcfg.poi_id_juviesrow, ewcfg.poi_id_copkilltown, ewcfg.poi_id_rowdyroughhouse]
+	return user_data.poi not in [ewcfg.poi_id_thesewers, ewcfg.poi_id_juviesrow, ewcfg.poi_id_copkilltown, ewcfg.poi_id_rowdyroughhouse]
 
