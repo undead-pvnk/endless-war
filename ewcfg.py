a21b3e38892ddf3b5e08837cecc923521deae81e
miller199 <windmillerg@gmail.com>
2019-12-17 20:59:30 -0500

    added a couple zine lists

--- ewcfg.py
+++ ewcfg.py
@@ -14112,6 +14112,28 @@ pray_responses_list = [
 	"ENDLESS WAR helpfully reminds you that !harvest is not a valid text command.",
 ]
 
+# list of genres and aliases
+book_genres = [
+	"narrative", #0
+	"historical", #1
+	"comic", #2
+	"porn", #3
+	"instructional", #4
+	"lore", #5
+	"reference", #6
+	"journal", #7
+	]
+
+# rating flavor text
+rating_flavor = [
+	"",
+	"Seething with hatred",
+	"Teeming with disappointment",
+	"pullulating with mild satisfaction",
+	"Brimming with respect",
+	"Glowing with admiration",
+	]
+
 # lists of all the discord server objects served by bot, identified by the server id
 server_list = {}
 
