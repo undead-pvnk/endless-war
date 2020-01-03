d35ed5cc6392222ac2cd78c461b54e90c04e7672
Marc-André Reichelt <marc.reichelt94@gmail.com>
2020-01-03 19:06:03 +0100

    bump rev v3.20yoslimernalia

--- ewcfg.py
+++ ewcfg.py
@@ -25,7 +25,7 @@ from ewdungeons import EwDungeonScene
 import ewdebug
 
 # Global configuration options.
-version = "v3.19b-yoslimernalia"
+version = "v3.20yoslimernalia"
 
 dir_msgqueue = 'msgqueue'
 
