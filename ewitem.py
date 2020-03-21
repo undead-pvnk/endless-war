90638b4622ccad3707be5e00ba7cba974333c1a5
BuildTools <unconfigured@null.spigotmc.org>
2020-03-21 13:52:57 -0500

    Fixed a minor issue.

--- ewitem.py
+++ ewitem.py
@@ -1490,13 +1490,13 @@ async def returnsoul(cmd):
 async def squeeze(cmd):
 	usermodel = EwUser(member=cmd.message.author)
 	soul_inv = inventory(id_user=cmd.message.author.id, id_server=cmd.message.server.id, item_type_filter=ewcfg.it_cosmetic)
+
+	if usermodel.life_state == ewcfg.life_state_corpse:
+		response = "Alas, you lack the mortal appendages required to wring the slime out of someone's soul."
+		return await ewutils.send_message(cmd.client, cmd.message.channel, ewutils.formatMessage(cmd.message.author, response))
 	
 	if cmd.mentions_count <= 0:
 		response = "Specify a soul you want to squeeze the life out of."
-		return await ewutils.send_message(cmd.client, cmd.message.channel, ewutils.formatMessage(cmd.message.author, response))
-
-	if usermodel.life_state == ewcfg.life_state_corpse:
-		response = "Alas, the precious soul slips right through your fingers. Maybe if you had a mortal body of flesh and blood, you'd be able to wring some slime out of it."
 		return await ewutils.send_message(cmd.client, cmd.message.channel, ewutils.formatMessage(cmd.message.author, response))
 	
 	target = cmd.mentions[0]
