815dbb6485d61118e36e060ff1f0a0acc28de77d
EthanStraub <ethan.straub@gmail.com>
2020-03-30 11:55:30 -0500

    Traps can now be set, but not yet triggered.

--- ewitem.py
+++ ewitem.py
@@ -1119,22 +1119,25 @@ async def item_use(cmd):
 				item.persist()
 					
 			elif context == ewcfg.context_prankitem:
-				should_delete_item = False
+				item_action = ""
 				use_mention_displayname = False
 
 				if (ewutils.channel_name_is_poi(cmd.message.channel.name) == False) or (user_data.poi not in ewcfg.capturable_districts):
 					response = "You need to be on the city streets to unleash that prank item's full potential."
 				else:
 					if item.item_props['prank_type'] == ewcfg.prank_type_instantuse:
-						should_delete_item, response, use_mention_displayname = await ewprank.prank_item_effect_instantuse(cmd, item)
+						item_action, response, use_mention_displayname = await ewprank.prank_item_effect_instantuse(cmd, item)
 					elif item.item_props['prank_type'] == ewcfg.prank_type_response:
-						should_delete_item, response, use_mention_displayname = await ewprank.prank_item_effect_response(cmd, item)
+						item_action, response, use_mention_displayname = await ewprank.prank_item_effect_response(cmd, item)
 					elif item.item_props['prank_type'] == ewcfg.prank_type_trap:
-						should_delete_item, response, use_mention_displayname = await ewprank.prank_item_effect_trap(cmd, item)
+						item_action, response, use_mention_displayname = await ewprank.prank_item_effect_trap(cmd, item)
 						
-					if should_delete_item:
+					if item_action == "delete":
 						item_delete(item.id_item)
-				
+					elif item_action == "drop":
+						give_item(id_user=(user_data.poi + '_trap'), id_server=item.id_server, id_item=item.id_item)
+						print(item.item_props)
+
 		await ewutils.send_message(cmd.client, cmd.message.channel, ewutils.formatMessage((cmd.message.author if use_mention_displayname == False else cmd.mentions[0]), response))
 		await ewrolemgr.updateRoles(client = cmd.client, member = cmd.message.author)
 
