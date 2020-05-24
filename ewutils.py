a57c86ebd90df3e68d39d916e2cf8448d14df01c
EthanStraub <ethan.straub@gmail.com>
2020-05-24 12:54:07 -0500

    Temporarily changed database used in ewutils

--- ewutils.py
+++ ewutils.py
@@ -434,7 +434,7 @@ def databaseConnect():
 	if conn_info == None:
 		db_pool_id += 1
 		conn_info = {
-			'conn': MySQLdb.connect(host = "localhost", user = "rfck-bot", passwd = "rfck" , db = "rfckevent", charset = "utf8"),
+			'conn': MySQLdb.connect(host = "localhost", user = "rfck-bot", passwd = "rfck" , db = "rfck", charset = "utf8"),
 			'created': int(time.time()),
 			'count': 1,
 			'closed': False
