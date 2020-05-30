8d3d5b2ed653778ee326e258e7e0055cbbca8613
munchypizza <munchywearstinyhats@gmail.com>
2020-05-30 16:51:51 -0500

    oh, oops I'm on the master branch LOL, uhhh
    -Added new properties to the cosmetic class: str_onadorn (a new flavor text displayed when you adorn something.), stats (a dict of the stats it effects: attack, defense, speed), durability (how many times it can be hit in battle before exploding), size (how big it is, this effects how many hats you can have on at the same time)
    
    -+defense simply adds the number to your armor you get with hardenedsap
    -+attack does this: slimes_spent * (10 + attack amount)
    -+speed turns the stat into a float, moves the decimal over to the left two times and adds it to the movespeed. there's a normal way to say that but it's 3:36 and i don't care.
    
    -Removed !dedorn because now you just !adorn again. Unadorning removes your stats, obviously.
    
    -added cosmetic durability, which is a slime hp equivelent that takes the same slimes_damage you do every hit. when it reaches zero, it breaks.
    
    -Added the Bodega in Krak Bay. there, you can !sew to restore cosmetics to their original durability. It costs 4 * the durability you regain. bodega has a store for clothes,a nd a secret store for people with a lot of fashion.
    
    -added !fashion, cosmetics have styles and users have freshness ratings. the freshness is the combined freshness of all your hats and a bonus modifier if you have a cohesive outfit, aka 3/4s of your cosmetics are of a single type. made dressed to kill work if you have a cohesive outfit
    
    -leadeboard
    
    !inspecting your cosmetic gives you it's rarity, durability, stat metrics, space requirements, style and fashion
    
    -cleaned up Adorn and sew, and added a fuck tone of abiralbesjf;jrgejgo5t
    
    -the size of the cosmetic matters, bigger cosmetics take up more space so you can adorn less. this is for balance, bigger cosmetics will also be better
    
    -made all dusty old cosmetics without a 'fashion_style' in their props get updated to the new format
    
    -code optimize, flavor text for ewcosmetic

--- ewutils.py
+++ ewutils.py
@@ -1438,7 +1438,7 @@ def get_move_speed(user_data):
 	if ewcfg.mutation_id_fastmetabolism in mutations and user_data.hunger / user_data.get_hunger_max() < 0.4:
 		move_speed *= 1.33
 
-	move_speed += (float(user_data.speed / 100))
+	move_speed = round(move_speed * (1 + float(user_data.speed / 100)))
 
 	move_speed = max(0.1, move_speed)
 
