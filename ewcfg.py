eeb9a30ea2b3138b952f214d443388fdf021c0b7
huckleton <unconfigured@null.spigotmc.org>
2020-09-03 04:10:19 -0800

    fixed things. registers fine in database now

--- ewcfg.py
+++ ewcfg.py
@@ -23576,17 +23576,17 @@ for mutation in mutations:
 	mutations_map[mutation.id_mutation] = mutation
 	mutation_ids.add(mutation.id_mutation)
 
-quadrant_sloshed = "sloshed"
-quadrant_roseate = "roseate"
-quadrant_violacious = "violacious"
-quadrant_policitous = "policitous"
+quadrant_sloshed = "flushed"
+quadrant_roseate = "pale"
+quadrant_violacious = "caliginous"
+quadrant_policitous = "ashen"
 
 quadrant_ids = [
 	quadrant_sloshed,
 	quadrant_roseate,
 	quadrant_violacious,
 	quadrant_policitous
-	]
+]
 
 quadrants_map = {}
 
