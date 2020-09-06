214585d1f3ce3b9ced8fb8c715a2eaeeded9926f
MeatyLearner <44686266+MeatyLearner@users.noreply.github.com>
2020-09-06 22:58:10 +0100

    updated help text and stopped movement after joining a gvs op

--- ewcmd.py
+++ ewcmd.py
@@ -3229,6 +3229,7 @@ async def gvs_join_operation(cmd):
 				# Lock juveniles into the district for garden ops
 				if faction == ewcfg.psuedo_faction_gankers:
 					ewutils.active_restrictions[user_data.id_user] = 4
+					ewutils.moves_active[user_data.id_user] = 0
 				
 				# If there are no player-generated operations, then the bot will simply spawn in ones automatically.
 				enemyfaction = ewcfg.psuedo_faction_gankers if faction == ewcfg.psuedo_faction_shamblers else ewcfg.psuedo_faction_shamblers
