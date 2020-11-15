dc6a72658d869d08d91c1b3195f7a2cab0718f48
MeatyLearner <44686266+MeatyLearner@users.noreply.github.com>
2020-11-15 23:54:55 +0000

    changed how gellphones are looked for

--- ew.py
+++ ew.py
@@ -1122,14 +1122,29 @@ class EwUser:
 		return int(res)
 
 	def has_gellphone(self):
+		"""
 		gellphones = ewitem.find_item_all(item_search = ewcfg.item_id_gellphone, id_user = self.id_user, id_server = self.id_server, item_type_filter = ewcfg.it_item)
 
 		for phone in gellphones:
 			phone_data = ewitem.EwItem(id_item = phone.get('id_item'))
 			if phone_data.item_props.get('active') == 'true':
 				return True
-
-		return False
+		"""
+		data = ewutils.execute_sql_query(
+		"SELECT * FROM items WHERE {id_user} = '%s' AND {id_item} in (SELECT {id_item} FROM items_prop WHERE {name} = %s AND {value} = %s) AND id_item IN (SELECT id_item FROM items_prop WHERE {name} = %s AND {value} = %s)".format(
+			id_user = ewcfg.col_id_user,
+			id_item = ewcfg.col_id_item,
+			name = ewcfg.col_name,
+			value = ewcfg.col_value
+		),(
+			self.id_user,
+			"item_name",
+			ewcfg.item_id_gellphone,
+			"active",
+			"true"
+		))
+	
+		return len(data) > 0
 
 	""" Create a new EwUser and optionally retrieve it from the database. """
 	def __init__(self, ew_id = None, member = None, id_user = None, id_server = None, data_level = 0):
