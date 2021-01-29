a91ed8d2dc9b851bb64d55d1412bfa8e7f6c3bec
Crank <88crankman88@gmail.com>
2021-01-29 12:21:11 +1000

    Slimecorp now has announcements
    Made hunting announcements use hideout_channels instead of being hardcoded and made #slimecorp-comms a hideout channel, and added special lines for slimecorp time announcements.

--- ewhunting.py
+++ ewhunting.py
@@ -1268,8 +1268,9 @@ class EwEnemy:
 				
 				if new_poi not in ewcfg.outskirts:
 					gang_base_response = "There are reports of a powerful enemy roaming around {}.".format(new_poi_def.str_name)
-					resp_cont.add_channel_response(ewcfg.channel_rowdyroughhouse, gang_base_response)
-					resp_cont.add_channel_response(ewcfg.channel_copkilltown, gang_base_response)
+					channels = ewcfg.hideout_channels
+					for ch in channels:
+						resp_cont.add_channel_response(ch, gang_base_response)
 		finally:
 			self.persist()
 			return resp_cont
