32e55d8aa37673b59f7a70d3ecc5dec21a127ef3
aristotlepenguin <43047449+aristotlepenguin@users.noreply.github.com>
2021-03-22 20:04:43 -0500

    oops
    dd

--- ewutils.py
+++ ewutils.py
@@ -456,7 +456,7 @@ def databaseConnect():
 	if conn_info == None:
 		db_pool_id += 1
 		conn_info = {
-		'conn': MySQLdb.connect(host = "localhost", user = "root", passwd = "theapocolypse3" , db = ewcfg.database, charset = "utf8"),
+		'conn': MySQLdb.connect(host = "localhost", user = "rfck-bot", passwd = "rfck" , db = ewcfg.database, charset = "utf8"),
 			'created': int(time.time()),
 			'count': 1,
 			'closed': False
