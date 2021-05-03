296b4048d5daba354585442c9d6b7b628d5c0d7d
Marc-André Reichelt <marc.reichelt94@gmail.com>
2021-05-03 22:41:05 +0200

    moved role backend interface to backend package

--- ew/rolemgr.py
+++ ew/rolemgr.py
@@ -7,67 +7,9 @@ from .static import poi as poi_static
 
 from . import utils as ewutils
 from . import item as ewitem
-from .backend import core as bknd_core
 
 from .user import EwUser
-
-class EwRole:
-	id_server = -1
-	id_role = -1
-	name = ""
-
-	def __init__(self, id_server = None, name = None, id_role = None):
-		if id_server is not None and name is not None:
-			self.id_server = id_server
-			self.name = name
-
-
-			data = bknd_core.execute_sql_query("SELECT {id_role} FROM roles WHERE id_server = %s AND {name} = %s".format(
-				id_role = ewcfg.col_id_role,
-				name = ewcfg.col_role_name
-			), (
-				id_server,
-				name
-			))
-
-			if len(data) > 0:  # if data is not empty, i.e. it found an entry
-				# data is always a two-dimensional array and if we only fetch one row, we have to type data[0][x]
-				self.id_role = data[0][0]
-			else:  # create new entry
-				bknd_core.execute_sql_query("REPLACE INTO roles ({id_server}, {name}) VALUES (%s, %s)".format(
-					id_server = ewcfg.col_id_server,
-					name = ewcfg.col_role_name
-				), (
-					id_server,
-					name
-				))
-		elif id_server is not None and id_role is not None:
-			self.id_server = id_server
-			self.id_role = id_role
-
-
-			data = bknd_core.execute_sql_query("SELECT {name} FROM roles WHERE id_server = %s AND {id_role} = %s".format(
-				id_role = ewcfg.col_id_role,
-				name = ewcfg.col_role_name
-			), (
-				id_server,
-				id_role
-			))
-
-			if len(data) > 0:  # if data is not empty, i.e. it found an entry
-				# data is always a two-dimensional array and if we only fetch one row, we have to type data[0][x]
-				self.name = data[0][0]
-
-	def persist(self):
-		bknd_core.execute_sql_query("REPLACE INTO roles (id_server, {id_role}, {name}) VALUES(%s, %s, %s)".format(
-			id_role = ewcfg.col_id_role,
-			name = ewcfg.col_role_name
-		), (
-			self.id_server,
-			self.id_role,
-			self.name
-		))
-			
+from .backend.role import EwRole
 
 """
 	Find relevant roles and save them to the database.
