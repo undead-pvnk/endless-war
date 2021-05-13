179f1cc6450c3b9d7aea4d35a3387c9fbd5b56fc
Marc-André Reichelt <marc.reichelt94@gmail.com>
2021-05-13 23:48:03 +0200

    added nice format tests

--- test/utils/core_test.py
+++ test/utils/core_test.py
@@ -36,7 +36,30 @@ class VectorTest(unittest.TestCase):
 
 		self.assertEqual(vector.normalize().vector, [1, 0])
 
+class NiceFormatTest(unittest.TestCase):
 
+	def test_formatNiceList(self):
+
+		self.assertEqual(ewutils.formatNiceList(['a', 'b', 'c']), 'a, b, and c')
+		self.assertEqual(ewutils.formatNiceList(['a', 'b'], 'or'), 'a or b')
+
+	def test_formatNiceTime(self):
+
+		time_short = 23;
+		time_medium = 2 * 60 + 4;
+		time_long = 1 * 60 * 60 + 3 * 60 + 1
+
+		self.assertEqual(ewutils.formatNiceTime(time_short), '23 seconds')
+		self.assertEqual(ewutils.formatNiceTime(time_short, round_to_minutes=True), '0 minutes')
+		self.assertEqual(ewutils.formatNiceTime(time_short, round_to_hours=True), '0 hours')
+
+		self.assertEqual(ewutils.formatNiceTime(time_medium), '2 minutes and 4 seconds')
+		self.assertEqual(ewutils.formatNiceTime(time_medium, round_to_minutes=True), '2 minutes')
+		self.assertEqual(ewutils.formatNiceTime(time_medium, round_to_hours=True), '0 hours')
+
+		self.assertEqual(ewutils.formatNiceTime(time_long), '1 hour, 3 minutes, and 1 second')
+		self.assertEqual(ewutils.formatNiceTime(time_long, round_to_minutes=True), '1 hour and 3 minutes')
+		self.assertEqual(ewutils.formatNiceTime(time_long, round_to_hours=True), '1 hour')
 
 if __name__ == '__main__':
 	unittest.main()
