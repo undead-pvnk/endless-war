293e62c2813528c7a1730e11a0f8a654d21b2112
Froggg <rex.brittingham@gmail.com>
2021-06-16 00:33:49 -0500

    Reformatted ew/backend

--- ew/backend/slimeoid.py
+++ ew/backend/slimeoid.py
@@ -1,195 +1,196 @@
 from . import core as bknd_core
 from ..static import cfg as ewcfg
 
+
 class EwSlimeoidBase:
-	id_slimeoid = 0
-	id_user = ""
-	id_server = -1
+    id_slimeoid = 0
+    id_user = ""
+    id_server = -1
 
-	life_state = 0
-	body = ""
-	head = ""
-	legs = ""
-	armor = ""
-	weapon = ""
-	special = ""
-	ai = ""
-	sltype = "Lab"
-	name = ""
-	atk = 0
-	defense = 0
-	intel = 0
-	level = 0
-	time_defeated = 0
-	clout = 0
-	hue = ""
-	coating = ""
-	poi = ""
+    life_state = 0
+    body = ""
+    head = ""
+    legs = ""
+    armor = ""
+    weapon = ""
+    special = ""
+    ai = ""
+    sltype = "Lab"
+    name = ""
+    atk = 0
+    defense = 0
+    intel = 0
+    level = 0
+    time_defeated = 0
+    clout = 0
+    hue = ""
+    coating = ""
+    poi = ""
 
-	#slimeoid = EwSlimeoid(member = cmd.message.author, )
-	#slimeoid = EwSlimeoid(id_slimeoid = 12)
+    # slimeoid = EwSlimeoid(member = cmd.message.author, )
+    # slimeoid = EwSlimeoid(id_slimeoid = 12)
 
-	""" Load the slimeoid data for this user from the database. """
-	def __init__(self, member = None, id_slimeoid = None, life_state = None, id_user = None, id_server = None, sltype = "Lab", slimeoid_name = None):
-		query_suffix = ""
-		suffix_values = []
-		user_data = None
-		if member != None:
-			id_user = str(member.id)
-			id_server = member.guild.id
-		elif id_user != None:
-			id_user = str(id_user)
+    """ Load the slimeoid data for this user from the database. """
 
-		#	user_data = EwUser(member = member)
+    def __init__(self, member = None, id_slimeoid = None, life_state = None, id_user = None, id_server = None, sltype = "Lab", slimeoid_name = None):
+        query_suffix = ""
+        suffix_values = []
+        user_data = None
+        if member != None:
+            id_user = str(member.id)
+            id_server = member.guild.id
+        elif id_user != None:
+            id_user = str(id_user)
 
-		#if user_data != None:
-		#	if user_data.active_slimeoid > -1:
-		#		id_slimeoid = user_data.active_slimeoid
+        #	user_data = EwUser(member = member)
 
-		if id_slimeoid != None:
-			query_suffix = " WHERE id_slimeoid = %s"
-			suffix_values.append(id_slimeoid)
-		else:
+        # if user_data != None:
+        #	if user_data.active_slimeoid > -1:
+        #		id_slimeoid = user_data.active_slimeoid
 
-			if id_user != None and id_server != None:
-				query_suffix = " WHERE id_user = %s AND id_server = %s"
-				suffix_values.extend([id_user, id_server])
-				if life_state != None:
-					query_suffix += " AND life_state = %s"
-					suffix_values.append(life_state)
-				if sltype != None:
-					query_suffix += " AND type = %s"
-					suffix_values.append(sltype)
-				if slimeoid_name != None:
-					query_suffix += " AND NAME = %s"
-					suffix_values.append(slimeoid_name)
+        if id_slimeoid != None:
+            query_suffix = " WHERE id_slimeoid = %s"
+            suffix_values.append(id_slimeoid)
+        else:
 
+            if id_user != None and id_server != None:
+                query_suffix = " WHERE id_user = %s AND id_server = %s"
+                suffix_values.extend([id_user, id_server])
+                if life_state != None:
+                    query_suffix += " AND life_state = %s"
+                    suffix_values.append(life_state)
+                if sltype != None:
+                    query_suffix += " AND type = %s"
+                    suffix_values.append(sltype)
+                if slimeoid_name != None:
+                    query_suffix += " AND NAME = %s"
+                    suffix_values.append(slimeoid_name)
 
-		if query_suffix != "":
-			try:
-				conn_info = bknd_core.databaseConnect()
-				conn = conn_info.get('conn')
-				cursor = conn.cursor()
+        if query_suffix != "":
+            try:
+                conn_info = bknd_core.databaseConnect()
+                conn = conn_info.get('conn')
+                cursor = conn.cursor()
 
-				# Retrieve object
-				cursor.execute("SELECT {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {} FROM slimeoids{}".format(
-					ewcfg.col_id_slimeoid,
-					ewcfg.col_id_user,
-					ewcfg.col_id_server,
-					ewcfg.col_life_state,
-					ewcfg.col_body,
-					ewcfg.col_head,
-					ewcfg.col_legs,
-					ewcfg.col_armor,
-					ewcfg.col_weapon,
-					ewcfg.col_special,
-					ewcfg.col_ai,
-					ewcfg.col_type,
-					ewcfg.col_name,
-					ewcfg.col_atk,
-					ewcfg.col_defense,
-					ewcfg.col_intel,
-					ewcfg.col_level,
-					ewcfg.col_time_defeated,
-					ewcfg.col_clout,
-					ewcfg.col_hue,
-					ewcfg.col_coating,
-					ewcfg.col_poi,
-					query_suffix
-				),
-				suffix_values
-				)
-				result = cursor.fetchone()
+                # Retrieve object
+                cursor.execute("SELECT {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {} FROM slimeoids{}".format(
+                    ewcfg.col_id_slimeoid,
+                    ewcfg.col_id_user,
+                    ewcfg.col_id_server,
+                    ewcfg.col_life_state,
+                    ewcfg.col_body,
+                    ewcfg.col_head,
+                    ewcfg.col_legs,
+                    ewcfg.col_armor,
+                    ewcfg.col_weapon,
+                    ewcfg.col_special,
+                    ewcfg.col_ai,
+                    ewcfg.col_type,
+                    ewcfg.col_name,
+                    ewcfg.col_atk,
+                    ewcfg.col_defense,
+                    ewcfg.col_intel,
+                    ewcfg.col_level,
+                    ewcfg.col_time_defeated,
+                    ewcfg.col_clout,
+                    ewcfg.col_hue,
+                    ewcfg.col_coating,
+                    ewcfg.col_poi,
+                    query_suffix
+                ),
+                    suffix_values
+                )
+                result = cursor.fetchone()
 
-				if result != None:
-					# Record found: apply the data to this object.
-					self.id_slimeoid = result[0]
-					self.id_user = result[1]
-					self.id_server = result[2]
-					self.life_state = result[3]
-					self.body = result[4]
-					self.head = result[5]
-					self.legs = result[6]
-					self.armor = result[7]
-					self.weapon = result[8]
-					self.special = result[9]
-					self.ai= result[10]
-					self.sltype = result[11]
-					self.name = result[12]
-					self.atk = result[13]
-					self.defense = result[14]
-					self.intel = result[15]
-					self.level = result[16]
-					self.time_defeated = result[17]
-					self.clout = result[18]
-					self.hue = result[19]
-					self.coating = result[20]
-					self.poi = result[21]
+                if result != None:
+                    # Record found: apply the data to this object.
+                    self.id_slimeoid = result[0]
+                    self.id_user = result[1]
+                    self.id_server = result[2]
+                    self.life_state = result[3]
+                    self.body = result[4]
+                    self.head = result[5]
+                    self.legs = result[6]
+                    self.armor = result[7]
+                    self.weapon = result[8]
+                    self.special = result[9]
+                    self.ai = result[10]
+                    self.sltype = result[11]
+                    self.name = result[12]
+                    self.atk = result[13]
+                    self.defense = result[14]
+                    self.intel = result[15]
+                    self.level = result[16]
+                    self.time_defeated = result[17]
+                    self.clout = result[18]
+                    self.hue = result[19]
+                    self.coating = result[20]
+                    self.poi = result[21]
 
-			finally:
-				# Clean up the database handles.
-				cursor.close()
-				bknd_core.databaseClose(conn_info)
+            finally:
+                # Clean up the database handles.
+                cursor.close()
+                bknd_core.databaseClose(conn_info)
 
+    """ Save slimeoid data object to the database. """
 
-	""" Save slimeoid data object to the database. """
-	def persist(self):
-		try:
-			conn_info = bknd_core.databaseConnect()
-			conn = conn_info.get('conn')
-			cursor = conn.cursor()
+    def persist(self):
+        try:
+            conn_info = bknd_core.databaseConnect()
+            conn = conn_info.get('conn')
+            cursor = conn.cursor()
 
-			# Save the object.
-			cursor.execute("REPLACE INTO slimeoids({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)".format(
-				ewcfg.col_id_slimeoid,
-				ewcfg.col_id_user,
-				ewcfg.col_id_server,
-				ewcfg.col_life_state,
-				ewcfg.col_body,
-				ewcfg.col_head,
-				ewcfg.col_legs,
-				ewcfg.col_armor,
-				ewcfg.col_weapon,
-				ewcfg.col_special,
-				ewcfg.col_ai,
-				ewcfg.col_type,
-				ewcfg.col_name,
-				ewcfg.col_atk,
-				ewcfg.col_defense,
-				ewcfg.col_intel,
-				ewcfg.col_level,
-				ewcfg.col_time_defeated,
-				ewcfg.col_clout,
-				ewcfg.col_hue,
-				ewcfg.col_coating,
-				ewcfg.col_poi
-			), (
-				self.id_slimeoid,
-				self.id_user,
-				self.id_server,
-				self.life_state,
-				self.body,
-				self.head,
-				self.legs,
-				self.armor,
-				self.weapon,
-				self.special,
-				self.ai,
-				self.sltype,
-				self.name,
-				self.atk,
-				self.defense,
-				self.intel,
-				self.level,
-				self.time_defeated,
-				self.clout,
-				self.hue,
-				self.coating,
-				self.poi
-			))
+            # Save the object.
+            cursor.execute("REPLACE INTO slimeoids({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)".format(
+                ewcfg.col_id_slimeoid,
+                ewcfg.col_id_user,
+                ewcfg.col_id_server,
+                ewcfg.col_life_state,
+                ewcfg.col_body,
+                ewcfg.col_head,
+                ewcfg.col_legs,
+                ewcfg.col_armor,
+                ewcfg.col_weapon,
+                ewcfg.col_special,
+                ewcfg.col_ai,
+                ewcfg.col_type,
+                ewcfg.col_name,
+                ewcfg.col_atk,
+                ewcfg.col_defense,
+                ewcfg.col_intel,
+                ewcfg.col_level,
+                ewcfg.col_time_defeated,
+                ewcfg.col_clout,
+                ewcfg.col_hue,
+                ewcfg.col_coating,
+                ewcfg.col_poi
+            ), (
+                self.id_slimeoid,
+                self.id_user,
+                self.id_server,
+                self.life_state,
+                self.body,
+                self.head,
+                self.legs,
+                self.armor,
+                self.weapon,
+                self.special,
+                self.ai,
+                self.sltype,
+                self.name,
+                self.atk,
+                self.defense,
+                self.intel,
+                self.level,
+                self.time_defeated,
+                self.clout,
+                self.hue,
+                self.coating,
+                self.poi
+            ))
 
-			conn.commit()
-		finally:
-			# Clean up the database handles.
-			cursor.close()
-			bknd_core.databaseClose(conn_info)
+            conn.commit()
+        finally:
+            # Clean up the database handles.
+            cursor.close()
+            bknd_core.databaseClose(conn_info)
