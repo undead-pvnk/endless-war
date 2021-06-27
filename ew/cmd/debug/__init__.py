389e73b29148eebb9d955466c43face1f165463d
aristotlepenguin <43047449+aristotlepenguin@users.noreply.github.com>
2021-06-27 18:14:05 -0500

    a
    b

---
+++ ew/cmd/debug/__init__.py
@@ -0,0 +1,26 @@
+from ew.static import cfg as ewcfg
+from .debug import *
+
+cmd_map = {
+
+    # Inspect objects in a POI
+    ewcfg.cmd_scrutinize: debug.scrutinize,
+
+    # debug commands
+    # ewcfg.cmd_prefix + cmd_debug1: cmds.debug1,
+    # ewcfg.cmd_prefix + cmd_debug2: cmds.debug2,
+    # ewcfg.cmd_prefix + cmd_debug3: cmds.debug3,
+    # ewcfg.cmd_prefix + cmd_debug4: cmds.debug4,
+    # ewcfg.cmd_prefix + debug5: cmds.debug5,
+    # ewcfg.cmd_prefix + cmd_debug6: cmds.debug6,
+    # ewcfg.cmd_prefix + cmd_debug7: cmds.debug7,
+    # ewcfg.cmd_prefix + cmd_debug8: cmds.debug8,
+
+    ewcfg.cmd_prefix + debug.cmd_debug9: debug.debug9,
+
+    ewcfg.cmd_changegamestate: debug.change_gamestate,
+    ewcfg.cmd_display_states: debug.display_states,
+    # ewcfg.cmd_press_button: cmds.elevator_press,
+    # ewcfg.cmd_call_elevator: cmds.elevator_call,
+
+}
