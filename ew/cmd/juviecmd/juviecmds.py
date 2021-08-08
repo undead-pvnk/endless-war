bd4173a0f55319b6d11a4397f0067da10c55d296
Zugerujk <Popman7777@gmail.com>
2021-08-07 20:12:32 -0500

    Gross incompetence. GROSS.

--- ew/cmd/juviecmd/juviecmds.py
+++ ew/cmd/juviecmd/juviecmds.py
@@ -853,7 +853,7 @@ async def identify(cmd):
         response = "Cool. Noted. Enjoy your lack of gender, slime."
         user_data.gender = ""
         user_data.persist()
-    elif gender in ewcfg.curse_words:
+    elif ewcfg.curse_words in gender:
         response = "Hey, no matter what, you're still a juvenile. **NO SWEARS**."
     elif "\n" in gender:
         response = "No fucking line breaks! WTF!"    
