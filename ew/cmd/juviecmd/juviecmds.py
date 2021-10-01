e3c7e05d71ebbf4b71c15386236367dbde8cdc0e
mudkipslaps <37057242+mudkipslaps@users.noreply.github.com>
2021-10-01 15:46:41 -0500

    Dpick functions

--- ew/cmd/juviecmd/juviecmds.py
+++ ew/cmd/juviecmd/juviecmds.py
@@ -416,7 +416,7 @@ async def mine(cmd):
             if user_data.weapon >= 0:
                 weapon_item = EwItem(id_item=user_data.weapon)
                 weapon = static_weapons.weapon_map.get(weapon_item.item_props.get("weapon_type"))
-                if weapon.id_weapon == ewcfg.weapon_id_pickaxe and user_data.life_state != ewcfg.life_state_juvenile:
+                if (weapon.id_weapon == ewcfg.weapon_id_pickaxe or weapon.id_weapon == ewcfg.weapon_id_diamondpickaxe)  and user_data.life_state != ewcfg.life_state_juvenile:
                     has_pickaxe = True
             # if user_data.sidearm >= 0:
             #	sidearm_item = EwItem(id_item=user_data.sidearm)
