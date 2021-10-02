968177301d61574affdad9090a091e8f4e229064
Zugerujk <Popman7777@gmail.com>
2021-10-01 19:43:48 -0500

    lol juvie moment

--- ew/cmd/food/foodcmds.py
+++ ew/cmd/food/foodcmds.py
@@ -1,4 +1,5 @@
 import asyncio
+from ew.cmd.cmds.cmdcmds import partypoints
 import random
 import time
 
@@ -331,7 +332,7 @@ async def order(cmd):
 
                 controlling_faction = poi_utils.get_subzone_controlling_faction(user_data.poi, user_data.id_server)
 
-                if controlling_faction != "":
+                if controlling_faction != "" and current_vendor != ewcfg.vendor_atomicforestpp:
                     # prices are halved for the controlling gang
                     if controlling_faction == user_data.faction:
                         value /= 2
