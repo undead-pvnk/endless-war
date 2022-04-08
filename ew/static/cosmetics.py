6b7aa1af819c774e5f680d1cac5980bb3056aa8d
Zugerujk <Popman7777@gmail.com>
2022-04-07 23:50:46 -0500

    Adds !tagslimeoid to help text & dog tag to bazaar

--- ew/static/cosmetics.py
+++ ew/static/cosmetics.py
@@ -1323,7 +1323,7 @@ cosmetic_items_list = [
         str_onadorn = "You adorn the Dog Tag and get flashbacks to the trenches. If you die with these on, they aren't coming with you to the sewers.",
         durability = 1500,
         style = "tough",
-        vendors = ['Waffle House'],
+        vendors = ['Waffle House', 'bazaar'],
         price = 2000,
         freshness = 2,
     ),
