2cf0192ce72be520326efb20db15da23e4e312b0
GuardianGlimmer <74988752+GuardianGlimmer@users.noreply.github.com>
2022-06-02 00:31:23 -0500

    added Class EwChef and Chefs

--- ew/cmd/spooky/spookyutils.py
+++ ew/cmd/spooky/spookyutils.py
@@ -24,3 +24,20 @@ def generate_negaslimeoid_name():
     name = name.capitalize()
     full_name = "{}, {}".format(name, title)
     return full_name
+
+class EwChef:
+	cooking = False
+	serve = False
+	done = False
+	chef_id = 0
+	prompts = 0
+	reward = 0
+	def stop(self):
+		self.fishing = False
+		self.serve = False
+		self.done = False
+		self.chef_id = 0
+		self.prompts = 0
+		self.reward = 0
+		
+chefs = {}
